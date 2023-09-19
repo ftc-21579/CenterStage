36bb6299a39f2750c4845931e43f0e10873455de
astr0clad <colin.montigel@gmail.com>
2023-09-19 13:02:37 -0700

    Setup PhotonFTC for speeeeeedy loop times
    Also have gradle grab the latest version of quail so that way we don't need to manually update.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/JoosTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/JoosTestOpMode.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.amarcolini.joos.command.CommandOpMode;
+import com.outoftheboxrobotics.photoncore.PhotonCore;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.common.DiffySwerve;
@@ -10,8 +11,12 @@ public class JoosTestOpMode extends CommandOpMode {
 
     @Override
     public void preInit() {
+
+        PhotonCore.enable(); // Lynx module optimizations
+
         DiffySwerve robot = registerRobot(new DiffySwerve(telem));
 
         telem.addLine("Ready");
+
     }
 }
