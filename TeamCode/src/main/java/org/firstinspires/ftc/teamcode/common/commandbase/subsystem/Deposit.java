1850a891736d8383a7f67049e4fb276246b4a073
astr0clad <colin.montigel@gmail.com>
2023-11-10 22:14:55 -0700

    i swear i can code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/Deposit.java
@@ -58,7 +58,7 @@ public class Deposit {
         double liftPower = liftPID.getOutput(depositMotor.getCurrentPosition(), 0.5 * TICKS_PER_INCH);
         depositMotor.setPower(liftPower);
 
-        bot.telem.addData("Lift Power", 0.5 * TICKS_PER_INCH);
+        bot.telem.addData("Lift Setpoint", 0.5 * TICKS_PER_INCH);
         bot.telem.addData("Lift Position", depositMotor.getCurrentPosition() / TICKS_PER_INCH);
 
         new ReleasePixelsCommand(this).schedule();
