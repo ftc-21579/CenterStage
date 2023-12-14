7dde09236764b5e87dda7d6752601215cbfb7fcf
astr0clad <colin.montigel@gmail.com>
2023-12-14 16:08:26 -0700

    rr v0.5.6 and correct meepmeep
    that took a very very long time

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -1,10 +1,7 @@
 package com.example.meepmeep;
 
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -12,66 +9,27 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity botOne = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(66, 66, Math.toRadians(180), Math.toRadians(180), 12)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
                 .setDimensions(16, 17)
-                .build();
-
-        RoadRunnerBotEntity botTwo = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeBlueDark())
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(66, 66, Math.toRadians(180), Math.toRadians(180), 12)
-                .setDimensions(16, 17)
-                .build();
-
-        RoadRunnerBotEntity botThree = new DefaultBotBuilder(meepMeep)
-                .setColorScheme(new ColorSchemeRedLight())
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(66, 66, Math.toRadians(180), Math.toRadians(180), 12)
-                .setDimensions(16, 17)
-                .build();
-
-        botOne.runAction(botOne.getDrive().actionBuilder(new Pose2d(12, 64, Math.toRadians(90)))
-                .setReversed(true)
-                .splineTo(new Vector2d(7, 32), Math.toRadians(-135))
-                .waitSeconds(0.5)
-                .setReversed(false)
-                .setTangent(0)
-                .splineToSplineHeading(new Pose2d(48, 40, Math.toRadians(180)), Math.toRadians(0))
-                .waitSeconds(1)
-                .splineTo(new Vector2d(12, 12), Math.toRadians(180))
-                .splineTo(new Vector2d(-50, 12), Math.toRadians(180))
-                .waitSeconds(2)
-                .setReversed(true)
-                .splineTo(new Vector2d(12, 12), Math.toRadians(0))
-                .splineTo(new Vector2d(48, 36), Math.toRadians(0))
-                .waitSeconds(0.5)
-                .build());
-
-        botTwo.runAction(botTwo.getDrive().actionBuilder(new Pose2d(12, 64, Math.toRadians(90)))
-                .setReversed(true)
-                .lineToY(32)
-                .waitSeconds(0.5)
-                .setReversed(false)
-                .setTangent(0)
-                .splineToSplineHeading(new Pose2d(48, 36, Math.toRadians(180)), Math.toRadians(0))
-                .waitSeconds(0.5)
-                .splineTo(new Vector2d(12, 12), Math.toRadians(180))
-                .splineTo(new Vector2d(-50, 12), Math.toRadians(180))
-                .waitSeconds(1)
-                .setReversed(true)
-                .splineTo(new Vector2d(12, 12), Math.toRadians(0))
-                .splineTo(new Vector2d(48, 32), Math.toRadians(0))
-                .waitSeconds(0.5)
-                .build());
-
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .build()
+                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(botOne)
-                .addEntity(botTwo)
+                .addEntity(myBot)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/drive/roadrunner/trajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.common.drive.roadrunner.trajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
