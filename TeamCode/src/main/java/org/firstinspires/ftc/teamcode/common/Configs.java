34d9f01ede415b5c8405edc79463bb554a652068
astr0clad <colin.montigel@gmail.com>
2023-12-22 13:10:42 -0700

    also on purpose

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/Configs.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Configs {
+
+    /*
+        DEPOSIT
+     */
+
+    // Lift
+    public static double
+            liftBottomPosition = 2.0,
+            liftTransferPosition = -1.5,
+            liftHangHeightPosition = 21.0;
+
+    // Grippers
+    public static double
+            leftGripperGrabPosition = 0.25, leftGripperReleasePosition = 0.55,
+            rightGripperGrabPosition = 0.27, rightGripperReleasePosition = 0.57;
+
+    // V4B
+    public static double
+            leftV4bDepositPosition = 0.8, leftV4bTransferPosition = 0.0,
+            leftV4bIdlePosition = 0.5, leftV4bDropPosition = 1,
+            rightV4bDepositPosition = 0.2, rightV4bTransferPosition = 1,
+            rightV4bIdlePosition = 0.5, rightV4bDropPosition = 0;
+
+}
